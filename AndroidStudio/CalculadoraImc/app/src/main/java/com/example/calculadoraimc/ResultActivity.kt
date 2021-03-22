a7228a893e1646b8b390de6c51aeabf37b19ee12
Rafael Heck <rafaelheck7@gmail.com>
2021-03-22 11:47:53 -0300

    Comitando projeto IMC e excluindo arquivos desnecessários

---
+++ AndroidStudio/CalculadoraImc/app/src/main/java/com/example/calculadoraimc/ResultActivity.kt
@@ -0,0 +1,14 @@
+package com.example.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        Log.w("lifecycle", "CREATE - estou criando a tela")
+    }
+
+}
