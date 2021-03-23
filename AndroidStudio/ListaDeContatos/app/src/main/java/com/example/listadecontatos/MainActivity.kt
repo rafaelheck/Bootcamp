61218fc28fac91f919b5741c7b032276583d644d
Rafael Heck <rafaelheck7@gmail.com>
2021-03-23 15:45:40 -0300

    Adicionando projeto Contatos

---
+++ AndroidStudio/ListaDeContatos/app/src/main/java/com/example/listadecontatos/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.listadecontatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bindViews()
+        updateList()
+    }
+
+    private fun bindViews(){
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList(){
+        adapter.updateList(arrayListOf(Contact("Rafael Heck", "(48) 99950-9333", "img.png"), Contact("Rafael Heck", "(48) 99950-9333", "img.png")))
+
+    }
+}
