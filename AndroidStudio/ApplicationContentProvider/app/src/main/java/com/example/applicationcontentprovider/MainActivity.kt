2668e35b259c601a3f8256c86215d2e86b54e28d
Rafael Heck <rafaelheck7@gmail.com>
2021-03-25 14:01:45 -0300

    ApplicationContentProvider v2

--- AndroidStudio/ApplicationContentProvider/app/src/main/java/com/example/applicationcontentprovider/MainActivity.kt
+++ AndroidStudio/ApplicationContentProvider/app/src/main/java/com/example/applicationcontentprovider/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.applicationcontentprovider
 
+import android.database.Cursor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
+import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.applicationcontentprovider.database.NotesDatabaseHelper.Companion.TITLE_NOTES
+import com.example.applicationcontentprovider.database.NotesProvider.Companion.URI_NOTES
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> {
+    lateinit var noteRecyclerView: RecyclerView
+    lateinit var noteAdd: FloatingActionButton
+
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        noteAdd = findViewById(R.id.note_add)
+        noteAdd.setOnClickListener{}
+
+        adapter = NotesAdapter()
+        adapter.setHasStableIds(true)
+
+
+        noteRecyclerView = findViewById(R.id.notes_recycler)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
+    }
+
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> = CursorLoader(this, URI_NOTES, null, null, null, TITLE_NOTES)
+
+    override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
+        if (data != null){ }
+    }
+
+    override fun onLoaderReset(loader: Loader<Cursor>) {
+        TODO("Not yet implemented")
     }
 }
